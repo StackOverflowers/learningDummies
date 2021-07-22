3c146bded2eb811632831e52c18332976b4ba5db
Anthony Ahumada <locopala99@gmail.com>
2021-07-22 11:19:30 +0200

    More

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,49 @@
-/* scripts  */
+var contador = 1 ;
+
+
+//funcion de tiempo
+
+setInterval(function(){
+    //se obtiene el elemento radio
+    document.getElementById('radio' + contador).checked = true;
+    //contador vamos a ir incrementando
+    contador ++;
+    //si contador es mayor a la cantidad de imagenes.
+    //contador se restablece a 1.
+    if(contador>6){
+        contador = 1;
+    }
+    //tiempo de las imagenes
+},5000);
+
+
+
+const bienvenida = document.getElementById('bienvenida');
+
+function actualizarNombre(){
+    let name = prompt("Por favor Introduzca su nombre");
+    name = name.toLocaleLowerCase();
+    if(name===null || name===""){
+        validar();
+        alert("No ingreso autenticacion");
+    }
+    bienvenida.textContent="Bienvenido : " + "" + name;
+    localStorage.setItem("name",name);
+}
+
+function validar(){
+    if(localStorage.getItem("name")){
+        bienvenida.textContent="Bienvenido : " + "" + localStorage.getItem("name");
+    }
+    else{
+        bienvenida.textContent="Bienvenido : " + "" + "sin autenticar";
+    }
+}
+
+validar();
+
+actualizarNombre();
+
+
+
+
