72518fa795ea99faf32672883755ba494a642c7b
Diana Chacon <dchacon871@unab.edu.co>
2021-07-23 09:33:25 -0500

    Update button 'ocultar'

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,10 +1,9 @@
-
-//obetengo por medio de queryselector todos los elementos en forma de array
+//Obtengo por medio de queryselector todos los elementos en forma de array
 let leer = document.querySelectorAll('.leer');
 
-//por medio de un bucle for recorro todos los botones de la pagina
+//por medio de un bucle for recorro todos los botones de la página
 for(let i = 0 ; i<leer.length;i++){
-    //obtengo todos los parrafos a los cuales quiero ocultar , tambien se manejan en forma de array
+    //Obtengo todos los parrafos a los cuales quiero ocultar, también se manejan en forma de array
     let parrafos = document.querySelectorAll('.parrafo-post');
     //a cada boton despes de dar clic se ejecuta la funcion
     leer[i].onclick=function (e){
@@ -15,11 +14,10 @@ for(let i = 0 ; i<leer.length;i++){
             parrafos[i].style.display="block";
             leer[i].textContent="Ocultar";
             leer[i].classList.remove("mystyle");
-        }//pero si si tiene el estilo block se oculta y cambia el texto del boton
+        }//pero si tiene el estilo block se oculta y cambia el texto del boton
         else{
             parrafos[i].style.display='none';
             leer[i].textContent="Mostrar";
-            
             leer[i].classList.add("mystyle");
         }
     }
