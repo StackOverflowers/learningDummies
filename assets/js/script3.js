abda940b2292b5892fb238ef9e1518342ce7c139
Anthony Ahumada <locopala99@gmail.com>
2021-07-26 08:51:24 +0200

    adding date script for each publication

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -12,17 +12,44 @@ for(let i = 0 ; i<leer.length;i++){
         //si parrafos en la posicion de i tiene el style=none , se muestra el parrafo
         //se añaden clases y se remueven clases
         if(parrafos[i].style.display==='none'){
+            leer[i].classList.remove("mystyle");
             parrafos[i].style.display="block";
             leer[i].textContent="Ocultar";
-            leer[i].classList.remove("mystyle");
+            
         }//pero si si tiene el estilo block se oculta y cambia el texto del boton
         else{
+            leer[i].classList.add("mystyle");
             parrafos[i].style.display='none';
             leer[i].textContent="Mostrar";
             
-            leer[i].classList.add("mystyle");
+           
         }
     }
 }
 
-   
+fecha();
+
+function fecha(){
+    let publicaciones = document.querySelectorAll('.datos-publicaciones1');
+    
+    
+
+
+    for(let i = 0;i<publicaciones.length;i++){
+    
+    
+    let date = new Date()
+
+    let day = date.getDate()
+    let month = date.getMonth() + 1
+    let year = date.getFullYear()
+    
+    if(month < 10){
+     publicaciones[i].innerHTML=`${day}-0${month}-${year}`
+     publicaciones[i].classList.add("datos-publicaciones");
+    }else{
+     publicaciones[i].innerHTML= `${day}-${month}-${year}`
+     publicaciones[i].classList.add("datos-publicaciones");
+    }
+    }
+}   
